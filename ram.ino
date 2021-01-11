48386e1f444d9cdde27c22140379ab7e1982a1e3
debobbin <you@example.com>
2021-01-11 02:00:16 +0100

    messing with visuals

--- ram.ino
+++ ram.ino
@@ -29,11 +29,58 @@ void fill_ram()
   writeRam(vga_data, ram_addr, sizeof(vga_data));
 }
 
+unsigned int i = 0;
+int n_zero = 0;
+int last_read = 0;
+int last_audio_val = 0;
+
+ISR(TIMER4_COMPA_vect){
+    //Pretty wonky setup to get some different results depending on type of sound, the higher the sound, the lower the number-ish, max is around 1100
+    int val = analogRead(A0);
+    if (last_read == 0 && val == 0){
+      n_zero ++;
+    } else if (val !=0) {
+       if (n_zero != 0){
+        last_read = n_zero;
+        last_audio_val = last_read;
+       }
+       n_zero = 0;
+
+    }
+    last_read = val;
+  
+//  int val = analogRead(A0);
+//  //int val = 1023;
+//  Serial.write(val & 0xFF);
+//  Serial.write(val >> 8 & 0xFF);
+//  Serial.write(val >> 16 & 0xFF);
+//  Serial.write(val >> 24 & 0xFF);
+}
+
+void setup_clock4(){
+  cli();
+  
+  // interrupt messes alot with the VGA output timing :/
+  TCCR4A = 0;
+  TCCR4B = 0;
+  TCNT4  = 0;
+  OCR4A = 50;
+  // turn on CTC mode
+  TCCR4B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR4B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK4 |= (1 << OCIE4A);
+
+  sei();
+}
+
 void setup()
 {
+  setup_clock4();
   vga.begin();
   initRam();
-  fill_ram();
+  //fill_ram();
   readRam(vgaxfb, ram_addr, VGAX_HEIGHT * VGAX_BWIDTH);
 }
 
@@ -57,22 +104,33 @@ byte audio_mem(bool hyper)
   memcpy(vgaxfb, memptr, VGAX_HEIGHT * VGAX_BWIDTH);
 }
 
+int last_used_audio_val = 0;
+bool stay = false;
+int stay_n = 1000;
 void loop()
 {
   byte x = rand() % VGAX_WIDTH;
   byte y = rand() % VGAX_HEIGHT;
   byte pix = vga.getpixel(x, y);
 
- //TODO: base on actual audio input
-  int val = analogRead(A0);
-  if (val < 500 && val > 20) {
-    // constantly restarting gives intense glitchy output, also seems to corrupt something because it can result in smaller glitches which stay until vga.begin() is called again?
-    vga.begin();
-  }
   
-  if (val > 500) {
-    //TODO: make button to stay in this mode, perhaps alternate hyper on and off?
-    audio_mem(false);
+  if (last_used_audio_val != last_audio_val){
+    if (last_audio_val > 7 || stay) {
+      stay = true;
+      audio_mem(true);
+      stay_n --;
+      if (stay_n == 0){
+        stay_n = 10;
+        stay = false;
+      }
+    }
+    
+    if (last_audio_val > 10) {
+      // constantly restarting gives intense glitchy output, also seems to corrupt something because it can result in smaller glitches which stay until vga.begin() is called again?
+      //vga.begin();
+       readRam(vgaxfb, ram_addr, VGAX_HEIGHT * VGAX_BWIDTH);
+    }
+    last_used_audio_val = last_audio_val;
   }
 
   //TODO: maybe something faster? would also be nice if speed based on audio
