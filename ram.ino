87fb66226ae149342f8c38b8c2519dcd89d93996
debobbin <you@example.com>
2021-01-16 02:34:14 +0100

    removed input tests

--- ram.ino
+++ ram.ino
@@ -2,105 +2,117 @@
 #include <VGAX.h>
 
 int ram_addr = 0;
-int n_entries = 500;
+int n_entries = 5;
 
 VGAX vga;
 
 void fill_ram()
 {
-  byte vga_data[VGAX_HEIGHT * VGAX_BWIDTH];
+  const int VGA_BWIDTH = 30;
+  const int VGA_HEIGHT = 60;
+  const int VGA_WIDTH = VGA_BWIDTH*4;
+  byte vga_data[VGA_HEIGHT*VGA_BWIDTH];
   memset(vga_data, 0, sizeof(vga_data));
-
-  for (int i = 0; i < n_entries; i++) {
-    byte x = rand() % 30 * 4;
-    byte y = rand() % VGAX_HEIGHT;
-    byte clr = rand() % 3 + 1;
-
-    //https://github.com/smaffer/vgax/blob/master/VGAX.h
-    byte *p = vga_data + y * VGAX_BWIDTH + (x >> 2);
-    byte bitpos = 6 - (x & 3) * 2;
-    *p = (*p & ~(3 << bitpos)) | clr << bitpos;
+  
+  for (int i=0;i<n_entries;i++){
+    byte x = rand()%30 *4;
+    byte y = rand()%VGA_HEIGHT;
+    byte clr = rand()%3+1;
+    
+    https://github.com/smaffer/vgax/blob/master/VGAX.h
+    byte *p=vga_data + y*VGA_BWIDTH + (x>>2);
+    byte bitpos=6-(x & 3)*2;
+    *p=(*p & ~(3 <<bitpos)) | clr <<bitpos;
   }
-
+  
   initRam();
   writeRam(vga_data, ram_addr, sizeof(vga_data));
 }
 
-void setup()
-{
-  randomSeed(analogRead(A1));
+void setup()   
+{  
   vga.begin();
   initRam();
-  fill_ram();
-  readRam(vgaxfb, ram_addr, VGAX_HEIGHT * VGAX_BWIDTH);
+  //fill_ram();
+  readRam(vgaxfb, ram_addr, VGAX_HEIGHT*VGAX_BWIDTH);
 }
 
+byte vga_mess2()
+{
+  int val = analogRead(A0);
+  if (val < 500 && val > 20){
+    memcpy(vgaxfb, rand(), VGAX_HEIGHT*VGAX_BWIDTH);
+    delay(4000);
+  }
+}
 
-int init_wait = 100;
-int wait = init_wait;
 int memptr = 0;
-byte vga_mess(bool hyper)
+
+byte vga_mess()
 {
-  if (hyper) {
-    memptr += random(5) - 10;
+  int val = analogRead(A0);
+  if (val > 500){
+    memcpy(vgaxfb, memptr++, VGAX_HEIGHT*VGAX_BWIDTH);
+    delay(4000);
   }
-  else if (--wait == 0) {
-    wait = init_wait;
-    memptr += random(5) - 10;
-  }
-  memcpy(vgaxfb, memptr, VGAX_HEIGHT * VGAX_BWIDTH);
 }
 
-void loop()
-{
-  byte x = rand() % VGAX_WIDTH;
-  byte y = rand() % VGAX_HEIGHT;
+void loop()                    
+{ 
+  byte x = rand()%VGAX_WIDTH;
+  byte y = rand()%VGAX_HEIGHT;
   byte pix = vga.getpixel(x, y);
 
-  int potVal = analogRead(A0);
+  int val = analogRead(A0);
 
-  vga_mess(false);
-
-  
-
-  //TODO: maybe something faster? would also be nice if speed based on audio
-  if (pix != 0) {
-    if (x - 1 >= 0) {
-      vga.putpixel(x - 1, y, pix);
-    }
-    if (x + 1 < VGAX_WIDTH) {
-      vga.putpixel(x + 1, y, pix);
-    }
-    if (y - 1 >= 0) {
-      vga.putpixel(x, y - 1, pix);
-    }
-    if (y + 1 < VGAX_HEIGHT) {
-      vga.putpixel(x, y + 1, pix);
-    }
-
-    if (random(10) == 0) {
-      vga.putpixel(x, y, rand() % 3 + 1);
-    }
+  if (val > 500){
+    memcpy(vgaxfb, memptr++, VGAX_HEIGHT*VGAX_BWIDTH);
+    delay(rand() % 8000);
   }
 
-  //    for (int i=0;i<20;i++){
-  //      vga.putpixel(rand()%VGAX_WIDTH,rand()%VGAX_HEIGHT, 0);
-  //    }
+  if (val < 500 && val > 20){
+    memcpy(vgaxfb, rand(), VGAX_HEIGHT*VGAX_BWIDTH);
+    delay(rand() % 8000);
+  }
 
+  if (val < 300){
+    // constantly restarting gives intense glitchy output
+    vga.begin();
+  }
 
-  //    for (int y=0;y<VGAX_HEIGHT;y++){
-  //      for (int x=0;x<VGAX_WIDTH;x++){
-  //        byte pix = vga.getpixel(x, y);
-  //        switch(pix){
-  //          case 0: //black
-  //            break;
-  //          case 1: //red
-  //            break;
-  //          case 2: //green
-  //            break;
-  //          case 3: // yellow
-  //            break;
-  //        }
-  //      }
-  //    }
+  //TODO: maybe something faster/else?
+//  if (pix!=0){
+//    if (x-1 >= 0){ 
+//      vga.putpixel(x-1, y, pix);
+//    }
+//    if (x+1 < VGAX_WIDTH){
+//      vga.putpixel(x+1, y, pix);
+//    }
+//    if (y-1 >= 0){
+//      vga.putpixel(x, y-1, pix);
+//    }
+//    if (y+1 < VGAX_HEIGHT){
+//      vga.putpixel(x, y+1, pix);
+//    }
+//
+//    if (rand()%10==0){
+//      vga.putpixel(x,y,rand()%3+1);
+//    }
+//  }
+
+//  for (int y=0;y<VGAX_HEIGHT;y++){
+//    for (int x=0;x<VGAX_WIDTH;x++){
+//      byte pix = vga.getpixel(x, y);
+//      switch(pix){
+//        case 0: //black
+//          break;
+//        case 1: //red
+//          break;
+//        case 2: //green
+//          break;
+//        case 3: // yellow
+//          break;
+//      }
+//    }
+//  }
 }
