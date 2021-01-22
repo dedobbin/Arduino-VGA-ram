590ca14958fd52524add0d83383aab86fd5eb023
debobbin <you@example.com>
2021-01-23 00:35:31 +0100

    button wip

--- ram.ino
+++ ram.ino
@@ -1,5 +1,8 @@
-#include "sram.h"
+lly #include "sram.h"
 #include <VGAX.h>
+
+#define ANALOG_INPUT_PIN A0
+#define BUTTON_INPUT_PIN 8
 
 int ram_addr = 0;
 int n_entries = 5;
@@ -35,70 +38,58 @@ void setup()
   initRam();
   //fill_ram();
   readRam(vgaxfb, ram_addr, VGAX_HEIGHT*VGAX_BWIDTH);
-}
-
-byte vga_mess2()
-{
-  int val = analogRead(A0);
-  if (val < 500 && val > 20){
-    memcpy(vgaxfb, rand(), VGAX_HEIGHT*VGAX_BWIDTH);
-    delay(4000);
-  }
+  pinMode(BUTTON_INPUT_PIN, INPUT);
 }
 
 int memptr = 0;
 
-byte vga_mess()
-{
-  int val = analogRead(A0);
-  if (val > 500){
-    memcpy(vgaxfb, memptr++, VGAX_HEIGHT*VGAX_BWIDTH);
-    delay(4000);
-  }
-}
-
 void loop()                    
 { 
-  byte x = rand()%VGAX_WIDTH;
-  byte y = rand()%VGAX_HEIGHT;
-  byte pix = vga.getpixel(x, y);
-
-  int val = analogRead(A0);
-
-  if (val > 500){
-    memcpy(vgaxfb, memptr++, VGAX_HEIGHT*VGAX_BWIDTH);
-    delay(rand() % 8000);
+  //TODO: when button pressed, freeze
+  if (digitalRead(BUTTON_INPUT_PIN)){
+    
+    int val = analogRead(ANALOG_INPUT_PIN);
+  
+    if (val > 500){
+      memcpy(vgaxfb, memptr++, VGAX_HEIGHT*VGAX_BWIDTH);
+      delay(rand() % 4000);
+    }
+  
+    if (val < 500 && val > 20){
+      memcpy(vgaxfb, rand(), VGAX_HEIGHT*VGAX_BWIDTH);
+      delay(rand() % 4000);
+    }
+  
+    if (val < 300){
+      // constantly restarting gives intense glitchy output
+      vga.begin();
+    }
+  
+    //TODO: maybe something faster/else?
+    
+    byte x = rand()%VGAX_WIDTH;
+    byte y = rand()%VGAX_HEIGHT;
+    byte pix = vga.getpixel(x, y);
+    
+    if (pix!=0){
+      if (x-1 >= 0){ 
+        vga.putpixel(x-1, y, pix);
+      }
+      if (x+1 < VGAX_WIDTH){
+        vga.putpixel(x+1, y, pix);
+      }
+      if (y-1 >= 0){
+        vga.putpixel(x, y-1, pix);
+      }
+      if (y+1 < VGAX_HEIGHT){
+        vga.putpixel(x, y+1, pix);
+      }
+  
+      if (rand()%10==0){
+        vga.putpixel(x,y,rand()%3+1);
+      }
+    }
   }
-
-  if (val < 500 && val > 20){
-    memcpy(vgaxfb, rand(), VGAX_HEIGHT*VGAX_BWIDTH);
-    delay(rand() % 8000);
-  }
-
-  if (val < 300){
-    // constantly restarting gives intense glitchy output
-    vga.begin();
-  }
-
-  //TODO: maybe something faster/else?
-//  if (pix!=0){
-//    if (x-1 >= 0){ 
-//      vga.putpixel(x-1, y, pix);
-//    }
-//    if (x+1 < VGAX_WIDTH){
-//      vga.putpixel(x+1, y, pix);
-//    }
-//    if (y-1 >= 0){
-//      vga.putpixel(x, y-1, pix);
-//    }
-//    if (y+1 < VGAX_HEIGHT){
-//      vga.putpixel(x, y+1, pix);
-//    }
-//
-//    if (rand()%10==0){
-//      vga.putpixel(x,y,rand()%3+1);
-//    }
-//  }
 
 //  for (int y=0;y<VGAX_HEIGHT;y++){
 //    for (int x=0;x<VGAX_WIDTH;x++){
